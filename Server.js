eb6397ea44371e412ffb3b8f3169e1780bd62c7d
Wr3nch^-^ <116601168+JuanBravo1@users.noreply.github.com>
2024-03-09 01:00:40 -0600

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,194 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const mqtt = require("mqtt");
+// Modulos utilizados
+
+const app = express();
+const port = 8080;
+// Configuracion del puerto
+
+// Configurar middleware para analizar el cuerpo de las solicitudes HTTP
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Configurar CORS para permitir solicitudes desde cualquier origen
+app.use(cors());
+
+// URL de conexión a tu base de datos MongoDB Atlas
+//"mongodb+srv://notjm:tqsjTGz5oWJlOdm2@eco-nido.dbwpny9.mongodb.net/?retryWrites=true&w=majority&appName=Eco-Nido"
+// mongodb+srv://notjm:tqsjTGz5oWJlOdm2@eco-nido.dbwpny9.mongodb.net/?retryWrites=true&w=majority&appName=Eco-Nido
+const mongoUrl = "mongodb+srv://greengarden:2MOdsHQX6cM8LDIO@greengarden.fjgd6uv.mongodb.net/?retryWrites=true&w=majority&appName=GreenGarden";
+
+// Cliente MQTT
+const mqttClient = mqtt.connect("mqtt://broker.hivemq.com");
+
+
+// Ruta para recibir datos desde la ESP32
+app.post('/insertDevice', async (req, res) => {
+  const data = req.body;
+
+  try {
+    // Conectar a la base de datos MongoDB Atlas
+    const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Conexión exitosa a MongoDB Atlas");
+
+    // Obtener una referencia a la base de datos y la colección
+    const db = client.db("greengarden");
+    const deviceCollection = db.collection("device");
+    const deviceHistoryCollection = db.collection("device_history");
+
+    const { mac } = data;
+    const existsDevice = await deviceCollection.findOne({ mac: mac });
+
+    // Comprobación de dispositivo
+    if (!existsDevice) {
+      // Insertar los datos en la colección si no existe
+      await deviceCollection.insertOne(data);
+    } else {
+      // Actualizar datos si ya existe el dispositivo
+      await deviceCollection.updateOne({ mac: mac }, { $set: data });
+    }
+
+    // Historial de dispositivo
+    await deviceHistoryCollection.insertOne(data);
+
+    // Cerrar la conexión
+    client.close();
+    console.log("Conexión cerrada");
+
+    // Responder a la ESP32 con un mensaje de confirmación
+    res.send("Datos recibidos y guardados en la base de datos");
+  } catch (error) {
+    console.error("Error al conectar a MongoDB Atlas:", error);
+    res.status(500).send("Error al conectar a la base de datos," + error);
+  }
+});
+
+app.post('/device/sensor', async (req, res) => {
+  const data = req.body;
+  try {
+    const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Conexion exitosa a MongoDB Atlas");
+
+    // Obtener una referencia a la base de datos y las colecciones
+    const db = client.db("greengarden");
+    const deviceCollection = db.collection("device");
+
+    // Primero se comprueba si no existe el usuario
+    const { mac } = data;
+    const exists = await deviceCollection.findOne({ mac: mac });
+
+    if (!exists) {
+      res.status(404).send("No se encontro el dispositivo:" + mac);
+    } else {
+      res.json(exists);
+    }
+
+    client.close();
+  } catch (error) {
+    console.error("Error al conectar MongoDB Atlas:", error);
+    res.status(500).send("Error al conectar a la base de datos");
+  }
+});
+
+// Ruta para recibir datos de mi ecoWeb (Registro de usuarios)
+app.post('/user', async (req, res) => {
+  const data = req.body;
+  let client; // Declarar la variable client fuera del bloque try para que pueda cerrarse en el bloque finally
+
+  try {
+    // Conectar a la base de datos MongoDBAtlas
+    client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Conexión exitosa a MongoDB Atlas");
+
+    // Obtener una referencia a la base de datos y las colecciones
+    const db = client.db("greengarden");
+    const userCollection = db.collection("users");
+
+    // Comprobar si no existe el usuario
+    const { username } = data;
+    const exists = await userCollection.findOne({ username: username });
+
+    if (!exists) {
+      await userCollection.insertOne(data);
+      res.send("Datos registrados satisfactoriamente");
+    } else {
+      res.send("Los datos enviados ya existen");
+    }
+
+    console.log("Datos recibidos del usuario");
+
+  } catch (error) {
+    console.error("Error al conectar MongoDB Atlas:", error);
+    res.status(500).send("Error al conectar a la base de datos");
+  } 
+});
+app.post('/user/login', async (req, res) => {
+  const data = req.body;
+  try {
+    // Validacion de datos
+    const { username, password } = data;
+    if (!username || !password) {
+      return res.status(400).send('Falta información de autenticación');
+    }
+
+    // Conectar a la base de datos MongoDBAtlas
+    const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Conexion exitosa a MongoDB Atlas");
+
+    // Obtener una referencia a la base de datos y las colecciones
+    const db = client.db("greengarden");
+    const userCollection = db.collection("users");
+
+    const exists = await userCollection.findOne({ username: username, password: password });
+    if (!exists) {
+      res.status(401).json({ status: false });
+    } else {
+      const { permisos, dispositivo } = exists;
+      res.status(200).json({ status: true, tipo: permisos, dispositivo: dispositivo });
+    }
+    client.close();
+
+  } catch (error) {
+    console.error("Error al conectar MongoDB Atlas:", error);
+    res.status(500).send("Error al conectar a la base de datos");
+  }
+})
+
+// Manejo MQTT peticiones
+const listen = (state) => {
+  mqttClient.publish('CATHY', state);
+  console.log(`Mensaje "${state}" enviado al topic "CATHY" satisfactoriamente`);
+};
+
+// Manejo MQTT POST
+app.post('/mqtt', (req, res) => {
+  console.log('Body:', req.body);
+
+  const { state } = req.body;
+  if (!state || !['lightON', 'lightOFF', 'fanON', 'fanOFF'].includes(state)) {
+    return res.status(400).send('Parámetros inválidos');
+  }
+
+  listen(state);
+  res.status(200).send('Mensaje recibido correctamente');
+});
+
+
+// Manejar errores 404 para rutas no encontradas
+app.use((req, res, next) => {
+  res.status(404).send("Ruta no encontrada");
+});
+
+// Manejar errores 500
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Error del servidor');
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor Node.js escuchando en http://localhost:${port}`);
+});
