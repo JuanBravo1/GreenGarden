d2f6343f66d709c36fc0e24781f7c105e197ce34
Wr3nch^-^ <116601168+JuanBravo1@users.noreply.github.com>
2024-03-14 12:09:34 -0600

    Update Server.js

--- Server.js
+++ Server.js
@@ -239,7 +239,7 @@ app.post('/mqtt', (req, res) => {
     return res.status(400).send('Parámetros inválidos');
   }
   
-  mqttClient.publish('CATHY', state);
+  mqttClient.publish('CATHY', "CLOSE");
   console.log(`Mensaje "${state}" enviado al topic "CATHY" satisfactoriamente`);
   res.status(200).send(state);
 });
